7aeb20c2addb62865444a5113c6b82e045420c42
Jon <jonatan.o.eriksson@gmail.com>
2022-07-24 16:57:59 +0200

    experimenting with vlc rc

---
+++ tcp_test.py
@@ -0,0 +1,29 @@
+import socket
+
+class Session:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(("127.0.0.1", 4444))
+        print(str(self._receive()))
+
+    def send_command(self, cmd: str) -> str:
+        msg = bytes(cmd + "\r\n", "utf-8")
+        self.s.sendall(msg)
+        return self._receive()
+
+    def _receive(self) -> str:
+        data = []
+        while True:
+            chunk = self.s.recv(2048)
+            data.append(chunk)
+            print(f"received {len(chunk)} bytes")
+
+            if b">" in chunk:
+                break
+
+        return (b''.join(data)).decode("utf-8")
+
+
+vlc_session = Session()
+print(vlc_session.send_command("help"))
+breakpoint()
