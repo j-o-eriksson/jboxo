5c244d7ced187f985887aae7a3af3b2ced92fc5c
jonatan.o.eriksson@gmail.com <jonatan.o.eriksson@gmail.com>
2023-07-23 13:47:00 -0500

    added web client

---
+++ client/app.ts
@@ -0,0 +1,72 @@
+const fetchVideos = async (): Promise<string> => {
+  const api = "http://192.168.0.189:5000/videos";
+  try {
+    const response = await fetch(api, { mode: "cors" });
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    if (error) {
+      return error.message;
+    }
+  }
+};
+
+const addVideo = async (path: string): Promise<string> => {
+  const api = "http://192.168.0.189:5000/control/add";
+  try {
+    const response = await fetch(api, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({ videoPath: path }),
+    });
+    const text = await response.text();
+    return text;
+  } catch (error) {
+    if (error) {
+      return error.message;
+    }
+  }
+};
+
+const stopVideo = async (): Promise<string> => {
+  const api = "http://192.168.0.189:5000/control/stop";
+  try {
+    const response = await fetch(api, {
+      method: "POST",
+      mode: "cors",
+    });
+    const text = await response.text();
+    console.log(`stop video returned ${text}`);
+    return text;
+  } catch (error) {
+    if (error) {
+      return error.message;
+    }
+  }
+};
+
+(async () => {
+  let stuff = await fetchVideos();
+  console.log(stuff);
+
+  let list = document.getElementById("myList");
+  for (let item of stuff) {
+    let li = document.createElement("li");
+    li.innerText = item["name"];
+    li.setAttribute("path", item["path"]);
+    list?.appendChild(li);
+  }
+
+  list?.addEventListener("click", (e) => {
+    const item = e.target as HTMLElement;
+    let path: string = item.getAttribute("path");
+    console.log(
+      `Clicked list element with name ${item.innerText} and content ${path}`,
+    );
+
+    (async () => {
+      let response = await addVideo(path);
+      console.log(response);
+    })();
+  });
+})();
