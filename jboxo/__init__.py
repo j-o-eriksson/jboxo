d3df598fa720be61dd6398b43e655de326183437
jon <jonatan.o.eriksson@gmail.com>
2023-07-28 11:34:00 -0700

    backend subtitle bugfix

--- jboxo/__init__.py
+++ jboxo/__init__.py
@@ -38,12 +38,6 @@ class VLCWrapper:
     def execute(self, cmd: str) -> None:
         self.m[cmd]()
 
-    def _get_paths(self, extentions):
-        files = (f for f in self.root.rglob("*") if f.suffix in extentions)
-        return {
-            "data": [{"name": _clean_string(p.stem), "path": str(p)} for p in files]
-        }
-
     def get_videos(self):
         return self._get_paths({".mp4"})
 
@@ -56,6 +50,12 @@ class VLCWrapper:
             "subtitle_name": self.video_meta.subtitle_name,
             "video_duration": self.video_meta.duration,
             "video_duration_str": str(timedelta(seconds=self.video_meta.duration)),
+        }
+
+    def _get_paths(self, extentions):
+        files = (f for f in self.root.rglob("*") if f.suffix in extentions)
+        return {
+            "data": [{"name": _clean_string(p.stem), "path": str(p)} for p in files]
         }
 
     def _add(self) -> None:
@@ -74,7 +74,7 @@ class VLCWrapper:
 
     def _play(self, seek_time: Optional[int] = None) -> None:
         if self.vlcprocess is not None:
-            self.vlcprocess.kill()
+            return
 
         if self.video_meta.video_path is None:
             raise ValueError("Video path not set.")
@@ -82,7 +82,7 @@ class VLCWrapper:
         cmd = f"cvlc '{str(self.video_meta.video_path)}' -f"
 
         if self.video_meta.subtitle_path is not None:
-            cmd += f" --sub-file 'str({self.video_meta.subtitle_path})'"
+            cmd += f" --sub-file '{str(self.video_meta.subtitle_path)}'"
 
         if seek_time is not None:
             cmd += f" --start-time={seek_time}"
