d9d438cd06e09cfb1279587aa9757cfb9380ec9c
jon <jonatan.o.eriksson@gmail.com>
2023-08-18 11:25:59 -0700

    move scripts to folder

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+scp frontend/build/index.html jon@raspberrypi:~/dev/jboxo/frontend/build
+scp frontend/build/static/js/* jon@raspberrypi:~/dev/jboxo/jboxo/static/js
+scp frontend/build/static/css/* jon@raspberrypi:~/dev/jboxo/jboxo/static/css
+
+# cp frontend/build/index.html jboxo/frontend/build
+# cp frontend/build/static/js/* jboxo/static/js
+# cp frontend/build/static/css/* jboxo/static/css
