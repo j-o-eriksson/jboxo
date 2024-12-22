44761bd68e03ac038e0a31567ca242141b08cbea
Jon <jonatan.o.eriksson@gmail.com>
2024-12-22 11:55:01 +0100

    vite react setup

--- jboxo/utils.py
+++ jboxo/utils.py
@@ -41,7 +41,6 @@ class VideoProvider:
 
 def clean_string(s: str) -> str:
     clean = s.translate({ord(c): " " for c in ".-[]"})
-
     MAX_LEN = 35
     return f"{clean[:MAX_LEN]}..." if len(clean) > MAX_LEN else clean
 
